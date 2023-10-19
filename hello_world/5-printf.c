c5ca656412a6b4429ce9d4e34874e22619d78ffd
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-19 11:38:54 -0700

    Added program that prints specific text ending with a new line at the end, using function printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Punto de entrada del programa
+ *
+ * Description: Imprime el mensaje y agrega una nueva linea al final
+ *
+ * Return: Siempre retorna 0
+ */
+int main(void)
+{
+  printf("swith proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
