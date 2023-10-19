c589e28a777b967e295b5bacc010f4b01b104fb8
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-19 08:30:20 -0700

    Added a c program that prints specific text ending with a new line

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main(void)
+int main(void)    /*Code to print "Programming is ... " followed by a new line*/
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+    return 0;
 }
