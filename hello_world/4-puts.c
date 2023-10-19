8ccd3ac7a1e9e20ebcca31a82a47a1c8150bd5b0
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-19 10:53:38 -0700

    Added a c program that prints specific text ending with a new line

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Punto de entrada del programa
+ *
+ * Description: Imprime el mensaje en la salida standard
+ *
+ * Return: Siempre cero
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
