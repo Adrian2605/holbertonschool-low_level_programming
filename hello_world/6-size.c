d2b422b1a19377d397cae3a8ba6ba953fde4bdfc
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-19 12:45:22 -0700

    Added program that prints the size of different data types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -20,7 +20,7 @@ size_t floatSize = sizeof(float);
 printf("Size of a char: %lu byte(s)\n", charSize);
 printf("Size of an int: %lu byte(s)\n", intSize);
 printf("Size of a long int: %lu byte(s)\n", longintSize);
-printf("Size of a long long int; %lu byte(s)\n", longlongintSize);
+printf("Size of a long long int: %lu byte(s)\n", longlongintSize);
 printf("Size of a float: %lu byte(s)\n", floatSize);
 
 return (0);
