fb7a5e5f9992b2008b7f9f6a875e0e7e77fde7e6
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-21 11:42:05 -0700

    Program that prints the alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the entire alphabet in lower case
+ * using while to be constantly printing unless it gets to z
+ * and then ends the loop and prints z.
+ *
+ * Return: Always return zero.
+ */
+int main (void);
+{
+char lowerletter = 'a';
+
+while (lowerletter != 'z')
+{
+putchar(lowerletter);
+lowerletter = lowerletter + 1;
+}
+putchar(lowerletter);
+return (0);
+}
