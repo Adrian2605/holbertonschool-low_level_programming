f62e77e474b61d2557a24f99316e91620f46f00e
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-21 22:15:04 -0700

    Program that prints all combinations of single digit numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: Prints all possible combinations for one single digit numbers.
+ *
+ * Return: Always zero.
+ */
+int main(void)
+{
+int number = 0;
+
+while (number <= 9)
+{
+putchar(number + '0');
+putchar(',');
+putchar(' ');
+number = number + 1;
+}
+putchar('\n');
+return (0);
+}
