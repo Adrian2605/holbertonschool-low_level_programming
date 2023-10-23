333c4ccd7307e42d1f693189d24c30eab7e5ed12
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-22 19:24:39 -0700

    Program that prints the alphabet with a function called print_alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,21 @@
-#include <main.h>
+#include <unistd.h>
 /**
  * main - Entry point.
  *
  * Return: Always zero.
  *
  */
+int print_alphabet()
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+write(1, &letter,1);
+letter = letter + 1;
+}
+return (0);
+}
+
 int main(void)
 {
 print_alphabet();
