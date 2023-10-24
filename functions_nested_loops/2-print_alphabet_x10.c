a37d5bf7b958c2499fc055365a271b4ac9256d46
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-24 16:54:39 -0700

    Function to print 10 times the alphabet followed by a new line

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,30 @@
  * print_alphabet_x10 - Prints the alphabet 10 times and prits a new line
  * at the end.
  *
+ * @c: Tha character to be printed.
+ *
  * Return: Always void.
  */
-
-void print_alphabet();
+int _putchar(char c);
 
 void print_alphabet_x10(void)
 {
 int i = 1;
 while (i <= 10)
 {
-print_alphabet();
+char letter = 'a';
+int counter = 0;
+
+while (counter < 1)
+{
+if (letter == '{' || letter == '\v')
+{
+letter = '\n';
+counter = counter + 1;
+}
+_putchar(letter);
+letter = letter + 1;
+}
 ++i;
 }
 return;
