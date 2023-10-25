a1780d06072a67c75ebe9573ff635613b46b3f83
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-25 14:38:03 -0700

    Function to print the last digit of a number

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,16 +2,34 @@
 
 /**
  * print_last_digit - Prints the last digit of a number.
+ * @c: The character to print.
  * @n: The number to extract the last digit from.
+ * @i: The number to get the absolute value from.
  *
  * Return: The value of the last digit.
  */
 
 int _putchar(char c);
 
+int _abs(int i)
+{
+int absolute_value;
+if (i < 0)
+{
+absolute_value = -i;
+return (absolute_value);
+}
+else
+{
+absolute_value = i;
+return (absolute_value);
+}
+}
+
 int print_last_digit(int n)
 {
-  int last_d = n % 10;
-  _putchar(last_d + '0');
-  return (last_d);
+n = _abs(n);
+int last_d = n % 10;
+_putchar(last_d + '0');
+return (last_d);
 }
