12cc6120fed9126f0fffd4b40311f63ed6353a99
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-26 06:21:01 -0700

    Function to print every minute in a day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,30 +1,28 @@
 #include <unistd.h>
 
 /**
- * jack_bauer - Prints all the minutes in the day of jack bauer.
+ * jack_bauer - Prints all the minutes in the day of Jack Bauer.
  *
  * Return: Always void.
  */
 
-
 void jack_bauer(void)
 {
-int hours, minutes;
-for (hours = 0; hours < 24; hours++)
-{
-for (minutes = 0; minutes < 60; minutes++)
-{
-char printed1 = (hours / 10) + '0';
-char printed2 = (hours % 10) + '0';
-char printed3 = (minutes / 10) + '0';
-char printed4 = (minutes % 10) + '0';
-write(1, printed1, 1);
-write(1, printed2, 1);
-write(1, ":", 1);
-write(1, printed3, 1);
-write(1, printed4, 1);
-write(1, "\n", 1);
-}
-}
-return;
+    int hours, minutes;
+    char printed[5];
+
+    for (hours = 0; hours < 24; hours++)
+    {
+        for (minutes = 0; minutes < 60; minutes++)
+        {
+            printed[0] = (hours / 10) + '0';
+            printed[1] = (hours % 10) + '0';
+            printed[2] = ':';
+            printed[3] = (minutes / 10) + '0';
+            printed[4] = (minutes % 10) + '0';
+
+            write(1, printed, 5);
+            write(1, "\n", 1);
+        }
+    }
 }
