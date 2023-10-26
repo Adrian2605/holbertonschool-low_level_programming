956ed077f6d61097ca6e81b5c6374ed35cad19b0
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-26 06:23:09 -0700

    Function to print every minute in a day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,21 +8,21 @@
 
 void jack_bauer(void)
 {
-    int hours, minutes;
-    char printed[5];
+int hours, minutes;
+char printed[5];
 
-    for (hours = 0; hours < 24; hours++)
-    {
-        for (minutes = 0; minutes < 60; minutes++)
-        {
-            printed[0] = (hours / 10) + '0';
-            printed[1] = (hours % 10) + '0';
-            printed[2] = ':';
-            printed[3] = (minutes / 10) + '0';
-            printed[4] = (minutes % 10) + '0';
+for (hours = 0; hours < 24; hours++)
+{
+for (minutes = 0; minutes < 60; minutes++)
+{
+printed[0] = (hours / 10) + '0';
+printed[1] = (hours % 10) + '0';
+printed[2] = ':';
+printed[3] = (minutes / 10) + '0';
+printed[4] = (minutes % 10) + '0';
 
-            write(1, printed, 5);
-            write(1, "\n", 1);
-        }
-    }
+write(1, printed, 5);
+write(1, "\n", 1);
+}
+}
 }
