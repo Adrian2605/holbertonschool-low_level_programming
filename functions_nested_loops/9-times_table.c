a3143ff68c11a8ea46b727015936fe9e72d1b8d0
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-27 07:11:17 -0700

    Function to print the 9 times table starting with 0

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,61 +8,60 @@
 
 void times_table(void)
 {
-  int ntable, nfile, product;
-  char p_product;
-  char p1_product;
-  char p2_product;
+int ntable, nfile, product;
+char p_product;
+char p1_product;
+char p2_product;
 
-  for (ntable = 0; ntable < 10; ++ntable)
-    {
-      for (nfile = 0; nfile < 10; ++nfile)
-        {
-          product = (ntable * nfile);
-          p_product = product + '0';
+for (ntable = 0; ntable < 10; ++ntable)
+{
+for (nfile = 0; nfile < 10; ++nfile)
+{
+product = (ntable * nfile);
+p_product = product + '0';
 
-          if (nfile == 0)
-          {
-            write(1, &p_product, 1);
-            write(1, ",", 1);
-            write(1, " ", 1);
-          }
-          else if ((nfile < 9) && (nfile > 0))
-          {
-	    if (product > 9)
+if (nfile == 0)
+{
+write(1, &p_product, 1);
+write(1, ",", 1);
+write(1, " ", 1);
+}
+else if ((nfile < 9) && (nfile > 0))
+{
+if (product > 9)
+{
+p1_product = (product / 10) + '0';
+p2_product = (product % 10) + '0';
+
+write(1, &p1_product, 1);
+write(1, &p2_product, 1);
+write(1, ",", 1);
+write(1, " ", 1);
+}
+else
 	      {
-		p1_product = (product / 10) + '0';
-		p2_product = (product % 10) + '0';
+write(1, " ", 1);
+write(1, &p_product, 1);
+write(1, ",", 1);
+write(1, " ", 1);
+}
+}
+else
+{
+if (product > 9)
+{
+p1_product = (product / 10) + '0';
+p2_product = (product % 10) + '0';
 
-		write(1, &p1_product, 1);
-		write(1, &p2_product, 1);
-		write(1, ",", 1);
-		write(1, " ", 1);
-	      }
-	    else
-	      {
-            write(1, " ", 1);
-            write(1, &p_product, 1);
-            write(1, ",", 1);
-            write(1, " ", 1);
-	      }
-          }
-          else
-          {
-	    if (product > 9)
-              {
-                p1_product = (product / 10) + '0';
-                p2_product = (product % 10) + '0';
-
-                write(1, " ", 1);
-                write(1, &p1_product, 1);
-                write(1, &p2_product, 1);
-              }
-	    else
-	      {
-            write(1, &p_product, 1);
-	      }
-          }
-        }
-      write(1, "\n", 1);
-    }
+write(1, &p1_product, 1);
+write(1, &p2_product, 1);
+}
+else
+{
+write(1, &p_product, 1);
+}
+}
+}
+write(1, "\n", 1);
+}
 }
