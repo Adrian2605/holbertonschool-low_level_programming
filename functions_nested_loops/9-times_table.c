2b7f51cb34c1b15735ddefe97487be4360c337b3
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-27 06:58:22 -0700

    Function to print the 9 times table starting with 0

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,6 +10,8 @@ void times_table(void)
 {
   int ntable, nfile, product;
   char p_product;
+  char p1_product;
+  char p2_product;
 
   for (ntable = 0; ntable < 10; ++ntable)
     {
@@ -26,14 +28,42 @@ void times_table(void)
           }
           else if ((nfile < 9) && (nfile > 0))
           {
+	    if (product > 9)
+	      {
+		p1_product = (product / 10) + '0';
+		p2_product = (product % 10) + '0';
+
+		write(1, " ", 1);
+		write(1, &p1_product, 1);
+		write(1, &p2_product, 1);
+		write(1, ",", 1);
+		write(1, " ", 1);
+	      }
+	    else
+	      {
             write(1, " ", 1);
             write(1, &p_product, 1);
             write(1, ",", 1);
             write(1, " ", 1);
+	      }
           }
           else
           {
+	    if (product > 9)
+              {
+                p1_product = (product / 10) + '0';
+                p2_product = (product % 10) + '0';
+
+                write(1, " ", 1);
+                write(1, &p1_product, 1);
+                write(1, &p2_product, 1);
+                write(1, ",", 1);
+                write(1, " ", 1);
+              }
+	    else
+	      {
             write(1, &p_product, 1);
+	      }
           }
         }
       write(1, "\n", 1);
