5de8d0cb864e44f98105a6387476bbad022e7a08
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-29 11:08:59 -0700

    Function that  takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * reset_to_98 -  takes a pointer to an int as parameter and updates
+ * updates the value it points to to 98.
+ * @*n: Pointer that the function takes as input.
+ *
+ * Return: Always void.
+ */
+
+void reset_to_98(int *n)
+{
+  *n = 98;
+
+  return;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+/**
+ * Prototypes of functions for tasks.
+ */
+
+int _putchar (c);
+
+void reset_to_98(int *n);
