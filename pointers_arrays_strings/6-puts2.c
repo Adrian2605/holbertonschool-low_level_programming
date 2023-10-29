6d6067876b896f0c2170e3c3321535b24e4b042f
Adrian Gallardo <adrian.gallardoro@hotmail.com>
2023-10-29 14:08:43 -0700

    Function that prints every other character of a string, starting with the first character, followed by a new line.

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+/**
+ * puts2 - Prints every other character of a string, starting with
+ * the first character, followed by a new line.
+ * @str: String to be evaluated.
+ *
+ * Return: Always void.
+ */
+
+void puts2(char *str)
+{
+int length = 0;
+int i;
+
+while (str[length] != '\0')
+{
+length++;
+}
+for (i = 0; i < length; i += 2)
+{
+write(1, str[i], 1);
+}
+write(1, "\n", 1);
+return;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,3 +16,4 @@ void print_rev(char *s);
 
 void rev_string(char *s);
 
+void puts2(char *str);
